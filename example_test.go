254d90799659af68bd47004a3dc11803c5966fbc
Oleiade <tcrevon@gmail.com>
2013-08-10 18:44:44 +0200

    Add: Tags function usage example to documentation

--- example_test.go
+++ example_test.go
@@ -62,3 +62,23 @@ func ExampleItems() {
     // field value map
     structItems, _ = reflections.Items(s)
 }
+
+func ExampleTags() {
+    s := MyStruct {
+        FirstField: "first value",      `matched:"first tag"`
+        SecondField: 2,                 `matched:"second tag"`
+        ThirdField: "third value",      `unmatched:"third tag"`
+    }
+
+    var structTags map[string]string
+
+    // Tags will return a field name to tag content
+    // map. Nota that only field with the tag name
+    // you've provided which will be matched.
+    // Here structTags will contain:
+    // {
+    //     "FirstField": "first tag",
+    //     "SecondField": "second tag",
+    // }
+    structTags, _ = reflections.Tags(s, "matched")
+}
