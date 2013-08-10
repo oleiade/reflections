58b5493e76debf8e9c4124c9d792912ad0da59e9
Oleiade <tcrevon@gmail.com>
2013-08-10 18:37:35 +0200

    Add : GetField example for documentation purposes

---
+++ example_test.go
@@ -0,0 +1,17 @@
+package reflection_test
+
+
+func ExampleGetField() {
+    s := MyStruct {
+        FirstField: "first value",
+        SecondField: 2,
+        ThirdField: "third value",
+    }
+
+    fieldsToExtract := []string{"FirstField", "ThirdField"}
+
+    for _, fieldName := range fieldsToExtract {
+        value, err := reflections.GetField(s, fieldName)
+        fmt.Println(value)
+    }
+}
