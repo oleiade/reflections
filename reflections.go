1adee11121a698e472a48577826a4e7a342b003e
Cliff Engle <cliffengle@gmail.com>
2013-08-11 00:40:53 -0700

    SetField: check type instead of kind.

--- reflections.go
+++ reflections.go
@@ -43,7 +43,6 @@ func SetField(obj interface{}, name string, value interface{}) error {
 	// Fetch the field reflect.Value
 	structValue := reflect.ValueOf(obj).Elem()
 	structFieldValue := structValue.FieldByName(name)
-	structFieldKind := structFieldValue.Kind()
 
 	if !structFieldValue.IsValid() {
 		return fmt.Errorf("No such field: %s in obj", name)
@@ -54,9 +53,10 @@ func SetField(obj interface{}, name string, value interface{}) error {
 		return fmt.Errorf("Cannot set %s field value", name)
 	}
 
+	structFieldType := structFieldValue.Type()
 	val := reflect.ValueOf(value)
-	invalidTypeError := errors.New("Provided value type didn't match obj field type")
-	if structFieldKind != val.Kind() {
+	if structFieldType != val.Type() {
+		invalidTypeError := errors.New("Provided value type didn't match obj field type")
 		return invalidTypeError
 	}
 
